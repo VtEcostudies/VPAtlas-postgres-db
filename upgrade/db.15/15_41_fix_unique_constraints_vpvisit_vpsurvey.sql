f60d8b1c2418e9057aaa8c24459487aaab6e5150
jloomisVCE <jloomis@vtecostudies.org>
2022-04-14 03:05:04 -0400

    fix unique constraints of vpvisit and vpsurvey to avoid dupes. automated vpsurvey duplicate delete.

---
+++ upgrade/db.15/15_41_fix_unique_constraints_vpvisit_vpsurvey.sql
@@ -0,0 +1,30 @@
+ALTER TABLE vpvisit DROP CONSTRAINT
+"vpVisit_unique_visitPoolId_Date_UserName_GlobalId";
+ALTER TABLE vpvisit ADD CONSTRAINT
+"vpVisit_unique_visitPoolId_Date_UserName"
+UNIQUE ("visitPoolId", "visitDate", "visitUserName");
+
+DELETE FROM vpsurvey WHERE "surveyId" IN (
+SELECT "maxSurveyId" FROM (
+	SELECT vs."surveyPoolId", vs."surveyTypeId", vs."surveyUserEmail", vs."surveyDate", vs."surveyTime", 
+	max(vs."surveyId") AS "maxSurveyId"
+	FROM vpsurvey vs JOIN
+		(SELECT "surveyPoolId", "surveyTypeId", "surveyUserEmail", "surveyDate", "surveyTime", count(*)
+		FROM "vpsurvey"
+		GROUP BY "surveyPoolId", "surveyTypeId", "surveyUserEmail", "surveyDate", "surveyTime"
+		HAVING count(*) > 1) 
+		AS dupes
+	ON dupes."surveyPoolId"=vs."surveyPoolId" 
+	AND dupes."surveyTypeId"=vs."surveyTypeId" 
+	AND dupes."surveyUserEmail"=vs."surveyUserEmail" 
+	AND dupes."surveyDate"=vs."surveyDate" 
+	AND dupes."surveyTime"=vs."surveyTime"
+	GROUP BY vs."surveyPoolId", vs."surveyTypeId", vs."surveyUserEmail", vs."surveyDate", vs."surveyTime"
+	) AS maxdupes
+);
+
+ALTER TABLE vpsurvey DROP CONSTRAINT
+"vpsurvey_unique_survey_PoolId_TypeId_Date_GlobalId";
+ALTER TABLE vpsurvey ADD CONSTRAINT
+"vpsurvey_unique_survey_PoolId_User_TypeId_Date_Time"
+UNIQUE ("surveyPoolId", "surveyTypeId", "surveyUserEmail", "surveyDate", "surveyTime");
